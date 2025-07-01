ccc36089f376cf02e3fb1e0edae21dac1d73fe4c
vinnyotach7 <vinnyotach7@gmail.com>
2025-07-01 14:21:56 +0300

    Update postcss.config.js

--- projectboltsb1eckdhrna/postcss.config.js
+++ projectboltsb1eckdhrna/postcss.config.js
@@ -1,6 +1,8 @@
-export default {
+module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    'postcss-import': {},
+    'tailwindcss': {},
+    'autoprefixer': {},
+    // add any other plugins here
   },
 };
